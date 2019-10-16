b7a797e8847f3a071ba5bc971a3d766165d24ea0
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-10-16 14:53:53 +0700

    :sparkles: Edit for testing

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const os = require('os');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/hostname', (req, res) => {
   res.send(`Hello ${os.hostname()} ${req.ip}`);
 });
 
-app.listen(3000, () => console.log('Running at port 3000'));
+var server = app.listen(3000, () => {
+  console.log("Listening on port " + server.address().port + "...");
+});
+
+var exports = module.exports = {};
+exports.close = function () {
+  server.close();
+}
